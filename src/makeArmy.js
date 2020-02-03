8148e5670072931771b774f591fa2f559a69cb90
Senya Volkov <formobileprima@gmail.com>
2020-02-03 13:04:22 +0200

    replaced variables

--- src/makeArmy.js
+++ src/makeArmy.js
@@ -1,28 +1,8 @@
 'use strict';
 
-/**
- * Implement makeArmy function:
- *
- * Function takes number (count) and generates an array with 'count' elements.
- * Each element is a function which returns its position in the array when
- * called.
- *
- * const shooters = makeArmy(10)
- *
- * shooters[0]() === 0;
- * shooters[5]() === 5;
- * shooters[9]() === 9;
- *
- * Don't use loop 'for' in the implementation. Try with 'while' and closures
- * instead.
- *
- * @param {number} count
- *
- * @return {function[]}
- */
 function makeArmy(count) {
   let amount = 0;
-  const arr = [];
+  const functionArr = [];
 
   while (amount <= count) {
     const check = amount;
@@ -31,11 +11,11 @@ function makeArmy(count) {
       return check;
     };
 
-    arr.push(item);
+    functionArr.push(item);
     amount++;
   }
 
-  return arr;
+  return functionArr;
 }
 
 module.exports = makeArmy;
